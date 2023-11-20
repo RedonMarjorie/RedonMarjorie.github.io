1df7afb36eda21f839c16f58af9114c01d19524f
RedonMarjorie <redon.marjorie@gmail.com>
2023-11-20 16:02:45 +0100

    original

--- projects/openseadragon/script.js
+++ projects/openseadragon/script.js
@@ -29,7 +29,7 @@ class Trigger extends React.Component {
       this.state.menuVisible ? /*#__PURE__*/
       React.createElement(DropDown, { title: "Digital representation of the Bayeux Tapestry - XIth century", onClick: this.handleClick.bind(this) }, /*#__PURE__*/
       React.createElement("p", null, "Credits : Ville de Bayeux, DRAC Normandie, Université de Caen Normandie, CNRS, ENS "),
-      React.createElement("p", null," Pictures : 2017 – La Fabrique de patrimoines en Normandie."),
+      React.createElement("a", {href: "https://www.bayeuxmuseum.com/la-tapisserie-de-bayeux/decouvrir-la-tapisserie-de-bayeux/explorer-la-tapisserie-de-bayeux-en-ligne/"}, "Link to the RBG panorama"),
       React.createElement("a", {href: "https://journals.openedition.org/insitu/21817"}, "RBG panorama reconstruction: Katia Zidane, Abderrahim El Moataz, Régis Clouard. The reconstruction of the Bayeux tapestry panorama as a reference tool for a spatialised system of documentary information. Image-processing algorithms devoted to panorama construction and plotting of images on this panorama. OpenEdition Journals"),
       React.createElement("p", null,""),
       React.createElement("a", {href: "https://hal.science/hal-04196665"}," Normal maps panorama generation: Marjorie Redon, Matthieu Pizenberg, Yvain Quéau, Abderrahim Elmoataz. 3D surface Approximation of the Entire Bayeux Tapestry for Improved Pedagogical Access. 4th ICCV Workshop on Electronic Cultural Heritage, Oct 2023, Paris, France.")) :
